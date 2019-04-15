3a6439b702efb26da3d5870b9c8530ca576b7174
yuanqingx <yuanqingx@aliyum.com>
2019-04-15 21:35:18 +0800

    add 添加博客

--- src/main/java/top/sicso/blog/admin/controller/AdminController.java
+++ src/main/java/top/sicso/blog/admin/controller/AdminController.java
@@ -1,14 +1,20 @@
 package top.sicso.blog.admin.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import top.sicso.blog.pojo.Blog;
 import top.sicso.blog.service.AdminService;
+import top.sicso.blog.service.BlogService;
+import top.sicso.blog.vo.BlogVO;
 
-@Api(value = "/", tags = "博客显示模块")
+@Api(value = "/", tags = "博客管理模块")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
@@ -16,8 +22,11 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @Autowired
+    private BlogService blogService;
+
     @ApiOperation(value = "加载管理系统首页", notes = "加载管理系统首页2")
-    @RequestMapping({"/", ""})
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
     public String index() {
         return "admin/index";
     }
@@ -40,5 +49,12 @@ public class AdminController {
         return "admin/project";
     }
 
+    @ApiOperation(value = "添加博客")
+    @ApiImplicitParam(name = "博客内容", value = "博客相关", required = true, paramType = "query", dataType = "top.sicso.blog.pojo.Blog")
+    @PostMapping("/addBlog")
+    public String addBlog(Blog blog) {
+
+        return "";
+    }
 
 }
--- src/main/java/top/sicso/blog/controller/IndexController.java
+++ src/main/java/top/sicso/blog/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import top.sicso.blog.service.AdminService;
 
@@ -38,7 +39,7 @@ public class IndexController {
     }
 
     @ApiOperation(value = "加载登录页面")
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
     public String loadLoginPage() {
         return "login";
     }
