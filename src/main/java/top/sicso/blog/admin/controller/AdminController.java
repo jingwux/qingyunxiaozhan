ef5b7d4612d56d732e4541c35061c07f087645f1
yuanqingx <yuanqingx@aliyum.com>
2019-04-15 20:35:26 +0800

    add 添加后台页面

--- src/main/java/top/sicso/blog/admin/controller/AdminController.java
+++ src/main/java/top/sicso/blog/admin/controller/AdminController.java
@@ -5,12 +5,8 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import top.sicso.blog.service.AdminService;
-
-import javax.servlet.http.HttpSession;
 
 @Api(value = "/", tags = "博客显示模块")
 @Controller
@@ -20,9 +16,28 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @ApiOperation(value = "加载管理系统首页", notes = "加载管理系统首页2")
     @RequestMapping({"/", ""})
     public String index() {
-        return "admin/monitor";
+        return "admin/index";
+    }
+
+    @ApiOperation(value = "加载发布博客页面", notes = "加载发布博客页面")
+    @GetMapping("/addBlogPage")
+    public String loadAddBlogPage() {
+        return "admin/addBlog";
+    }
+
+    @ApiOperation(value = "加载博客列表页面", notes = "加载发布博客页面")
+    @GetMapping("/listBlogPage")
+    public String loadBlogListPage() {
+        return "admin/listBlog";
+    }
+
+    @ApiOperation(value = "加载项目页面", notes = "加载发布博客页面")
+    @GetMapping("/projectPage")
+    public String loadProjectPage() {
+        return "admin/project";
     }
 
 
