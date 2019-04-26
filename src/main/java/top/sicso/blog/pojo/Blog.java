99ec1180aa20d8cc6871a0a0ded25e40cfb844dd
yuanqingx <yuanqingx@aliyun.com>
2019-04-26 12:29:57 +0800

    update blog pk id  to auto increment

--- src/main/java/top/sicso/blog/pojo/Blog.java
+++ src/main/java/top/sicso/blog/pojo/Blog.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class Blog {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;//id
 
     @Temporal(TemporalType.DATE)
