ce620eb79d347af480189e11ec2f3d1923330991
yuanqingx <yuanqingx@aliyun.com>
2019-04-27 11:08:08 +0800

    update file path DI

---
+++ src/main/java/top/sicso/blog/constant/FileHelper.java
@@ -0,0 +1,50 @@
+package top.sicso.blog.constant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileHelper {
+
+    public static String BASE_FILEPATH;
+
+    public static String EBOOK_FILEPATH;
+
+    public static String VIDEO;
+
+    public static String DOCUMENT;
+
+    public static String SOFTWARE;
+
+    public static String CRACK;
+
+    @Value("${base.filepath}")
+    public void setBaseFilepath(String baseFilepath) {
+        BASE_FILEPATH = baseFilepath;
+    }
+
+    @Value("${filepath.ebook}")
+    public void setEbookFilepath(String ebookFilepath) {
+        EBOOK_FILEPATH = ebookFilepath;
+    }
+
+    @Value("${filepath.video}")
+    public void setVIDEO(String VIDEO) {
+        FileHelper.VIDEO = VIDEO;
+    }
+
+    @Value("${filepath.document}")
+    public void setDOCUMENT(String DOCUMENT) {
+        FileHelper.DOCUMENT = DOCUMENT;
+    }
+
+    @Value("${filepath.software}")
+    public void setSOFTWARE(String SOFTWARE) {
+        FileHelper.SOFTWARE = SOFTWARE;
+    }
+
+    @Value("${filepath.crack}")
+    public void setCRACK(String CRACK) {
+        FileHelper.CRACK = CRACK;
+    }
+}
--- src/test/java/top/sicso/blog/MockTest.java
+++ src/test/java/top/sicso/blog/MockTest.java
@@ -39,7 +39,8 @@ public class MockTest {
                                      .andReturn();
 
         mvcResult.getModelAndView();
-        mvcResult.getResponse().getContentAsString();
+        String string = mvcResult.getResponse().getContentAsString();
+        System.out.println(string);
     }
 
 }
