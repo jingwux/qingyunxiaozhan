656ec177c4a43cc4851ecd9c2047397f25309346
yuanqingx <yuanqingx@aliyun.com>
2019-05-02 23:21:58 +0800

    add(resource) 添加资源控制器

--- src/main/java/top/sicso/blog/controller/ResourceController.java
+++ src/main/java/top/sicso/blog/controller/ResourceController.java
@@ -1,7 +1,24 @@
 package top.sicso.blog.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import top.sicso.blog.service.ResourceService;
 
+@Api(value = "/", tags = "资源显示模块")
 @Controller
 public class ResourceController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @ApiOperation(value = "获取资源列表")
+    @RequestMapping("/resource")
+    public String loadResourcePage(){
+        return "projects";
+    }
+
+
 }
