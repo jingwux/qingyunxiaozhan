b71a84764090508f13337f9026a41645520177e0
yuanqingx <yuanqingx@aliyun.com>
2019-05-03 14:20:40 +0800

    add(resources) 加载资源首页

--- src/main/java/top/sicso/blog/controller/ResourceController.java
+++ src/main/java/top/sicso/blog/controller/ResourceController.java
@@ -4,18 +4,20 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import top.sicso.blog.service.ResourceService;
 
 @Api(value = "/", tags = "资源显示模块")
 @Controller
+@RequestMapping("/resources")
 public class ResourceController {
 
     @Autowired
     private ResourceService resourceService;
 
-    @ApiOperation(value = "获取资源列表")
-    @RequestMapping("/resource")
+    @ApiOperation(value = "加载资源页面")
+    @GetMapping("")
     public String loadResourcePage(){
         return "projects";
     }
